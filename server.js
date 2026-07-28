180c94018f3854110802487b2a41c09d93d32515
cyrille0880 <c4807266@gmail.com>
2026-07-28 18:28:42 +0200

    init

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
+/**
+  * Routes
+  */
 app.get('/', (req, res) => {
-  res.send('Hello from Cyrille!');
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
+app.get('/organizations', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+});
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);
