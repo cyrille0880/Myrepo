d8284bf1468f7456b647c7ac45f7a4288aadc003
cyrille0880 <c4807266@gmail.com>
2026-08-11 15:07:20 +0200

    init

---
+++ src/server.js
@@ -0,0 +1,66 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import express from 'express';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
+import { getAllCategories } from './src/models/categories.js';
+
+// Define the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+/**
+ * Routes
+ */
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+
+
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
+});
+
+
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
+
+
+app.get('/categories', async (req, res) => {
+  const categories = await getAllCategories();
+    const title = '  categories';
+    res.render('categories', { title, categories });
+});
+
+
+
+
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+});
